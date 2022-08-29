cebb884ef49da3ad855cc8f71b6afd5723f63095
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2022-08-29 12:58:45 -0400

    Add files via upload
    Streamlined join_probability and implemented update() and normalize()

--- Week 3 - Uncertainty/heredity/heredity.py
+++ Week 3 - Uncertainty/heredity/heredity.py
@@ -1,5 +1,6 @@
 import csv
 import itertools
+from random import triangular
 import sys
 
 PROBS = {
@@ -139,66 +140,52 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    # goal is to get P(has gene) * P(has trait)
-    # we are given # of genes they have
-    # find if they have gene first, then get P(has trait) from PROBS dict
-    p = [] # list we'll store P(has gene) * P(has trait) in
-    # one_gene
-    for name in one_gene:
-        # case 1: has parents data
-        if people[name]["mother"] and people[name]["father"]:
-            parents = [people[name]["mother"], people[name]["father"]]
-            for parent in parents:
-                if parent in one_gene:
-                    genes = 1
-                elif parent in two_genes:
-                    genes = 2
-                else: genes = 0
-                if parent in have_trait:
-                    has_trait = True
-                else: has_trait = False
-                x = PROBS["gene"][genes]
-                y = PROBS["gene"][genes][has_trait]
-            p.append(x*y)    
+    p = [] # list we'll store each P(has gene) * P(has trait) in
+    
+    for name in people:
+        # get # of genes
+        if name in one_gene:
+            has_gene = 1
+        if name in two_genes:
+            has_gene = 2
+        else: has_gene = 0
+        # get trait: true/false
+        has_trait = name in have_trait
 
-        # case 2: no parents data
-        else:
-            x = PROBS["gene"][1]
-            if name in have_trait:
-                y = PROBS["trait"][1][True]
-            else:
-                y = PROBS["trait"][1][False]
-            p.append(x * y)
-    # Two genes
-    for name in two_genes:
-        # case 1: has parents data
-        if people[name]["mother"] and people[name]["father"]:
-            parents = [people[name]["mother"], people[name]["father"]]
-            for parent in parents:
-                if parent in one_gene:
-                    genes = 1
-                elif parent in two_genes:
-                    genes = 2
-                else: genes = 0
-                if parent in have_trait:
-                    has_trait = True
-                else: has_trait = False
-                x = PROBS["gene"][genes]
-                y = PROBS["gene"][genes][has_trait]
-            p.append(x*y)    
+        mother, father = people[name]["mother"], people[name]["father"]
+        if mother and father:
+            # getting probilities for inheriting from mother / father
+            if mother in two_genes:
+                p_mother = 1-PROBS["mutation"]
+            elif mother in one_gene:
+                p_mother = .5
+            else: p_mother = PROBS["mutation"]
 
-        # case 2: no parents data
-        else:
-            x = PROBS["gene"][2]
-            if name in have_trait:
-                y = PROBS["trait"][2][True]
+            if father in two_genes:
+                p_father = 1-PROBS["mutation"]
+            elif father in one_gene:
+                p_father = .5
+            else: p_father = PROBS["mutation"]
+
+            #calculating odds of getting gene from parents
+            if has_gene == 2:
+                x = p_mother*p_father
+            elif has_gene == 1:
+                x = (1- p_mother) * p_father + p_mother * (1 - p_father)
             else:
-                y = PROBS["trait"][2][False]
-            p.append(x * y)
+                x = (1 - p_mother) * (1 - p_father)
+            y = PROBS['trait'][has_gene][has_trait]
+            p.append(x*y)
+        else:
+            #no parents data
+            x = PROBS['gene'][has_gene]
+            y = PROBS['trait'][has_gene][has_trait]
+            p.append(x*y)
+
     # combine joint probabilities
     a = 1
     for i in p:
-        a = a*i
+        a *= i
     return a
 
 
@@ -209,7 +196,18 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for person in probabilities:
+        if person in one_gene:
+            has_gene = 1
+        elif person in two_genes:
+            has_gene = 2
+        else: has_gene = 0
+        if person in have_trait:
+            has_trait = True
+        else:
+            has_trait = False
+        probabilities[person]['gene'][has_gene] += p
+        probabilities[person]['trait'][has_trait] += p
 
 
 def normalize(probabilities):
@@ -217,7 +215,16 @@ def normalize(probabilities):
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    for person in probabilities:
+        # normalize genes, find total genes and divide each probability by total
+        total_gene = sum(probabilities[person]['gene'].values())
+        probabilities[person]['gene'] = {genes: pgene / total_gene for genes, pgene in probabilities[person]['gene'].items()}
+        
+        # normalize traits, find total traits and divide each probability by total
+        total_trait = sum(probabilities[person]['trait'].values())
+        probabilities[person]['trait'] = {trait: ptrait/total_trait for trait, ptrait in probabilities[person]['trait'].items()}
+        
+
 
 
 if __name__ == "__main__":
